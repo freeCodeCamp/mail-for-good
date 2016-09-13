46193dbff9648583645d310dd12997d4c728b841
4iar <me@4iar.com>
2016-09-13 21:32:10 +0100

    Fix: Add missing uri to endpoint

--- client/constants/endpoints.js
+++ client/constants/endpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'localhost:8080';
+export const BASE_URL = 'http://localhost:8080';
 
 export const SETTINGS_URL_RELATIVE = '/api/settings';
 export const SETTINGS_URL_WHOLE = BASE_URL + SETTINGS_URL_RELATIVE;
