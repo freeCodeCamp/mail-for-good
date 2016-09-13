005c4a5288bf71af4f5e899477df088e6b5f3ce3
4iar <me@4iar.com>
2016-09-13 18:25:04 +0100

    Add basic handling of SES credentials input
    Planning to move the POST request to redux. Should make the tests a bit
    cleaner too.

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
+
+        <Link to="settings">settings</Link>
+        {this.props.children}
       </div>
     );
   }
 }
+
+App.propTypes = {
+  children: React.PropTypes.element.isRequired
+};
