f0aa374600b2fdc4c7913d550c625f1a2714bde1
4iar <me@4iar.com>
2016-09-14 17:51:55 +0100

    Add subscriber list csv parsing function

---
+++ client/utils/subscriberListParsers/parseSubscriberList.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import Papa from 'papaparse';
+
+export default function parseSubscriberList(inputData, fields) {
+  // Parses a file (tsv/csv) to a format that can be recognised by the multiple subscriber api
+  //  {
+  //    fields: { fieldname1: fieldtype1, fieldname2: fieldtype2...},
+  //    data: {
+  //      { fieldname1: fieldname1_record1, fieldname2: fieldname2_record1 },
+  //      { fieldname1: fieldname1_record2, fieldname2: fieldname2_record2 },
+  //      { fieldname1: fieldname1_record3, fieldname2: fieldname2_record3 },
+  //       ...
+  //    }
+  //  }
+
+  // Convert the fields to a header line that Papa can parse into a
+  // value keyed object array, rather than an array of arrays
+  const header = _.keys(fields);
+
+  const data = Papa.parse(header + '\n' + inputData, { header: true });
+
+  return {data: data.data, fields};
+}
---
+++ client/utils/subscriberListParsers/parseSubscriberList.spec.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import parseSubscriberListCsv from './parseSubscriberList';
+
+const sampleDataNormalData = `nreid0@domainmarket.com,Female,Nancy,Reid,Virginia,Plaza
+bbrooks1@about.me,Female,Bonnie,Brooks,Florida,Circle
+khanson5@amazon.co.jp,Male,Keith,Hanson,California,Pass
+jgrant6@hexun.com,Female,Julia,Grant,Georgia,Drive`;
+
+const sampleDataNormalExpected = {
+  data: [
+    {email: 'nreid0@domainmarket.com', gender: 'Female', firstName: 'Nancy', lastName: 'Reid', state: 'Virginia', streetType: 'Plaza'},
+    {email: 'bbrooks1@about.me', gender: 'Female', firstName: 'Bonnie', lastName: 'Brooks', state: 'Florida', streetType: 'Circle'},
+    {email: 'khanson5@amazon.co.jp', gender: 'Male', firstName: 'Keith', lastName: 'Hanson', state: 'California', streetType: 'Pass'},
+    {email: 'jgrant6@hexun.com', gender: 'Female', firstName: 'Julia', lastName: 'Grant', state: 'Georgia', streetType: 'Drive'},
+  ],
+  fields: {email: 'email', gender: 'text', firstName: 'text', lastName: 'text', state: 'group', streetType: 'group'},
+}
+
+const sampleDataNormalFields = {
+  email: 'email',
+  gender: 'text',
+  firstName: 'text',
+  lastName: 'text',
+  state: 'group',
+  streetType: 'group'
+}
+
+describe('parseSubscriberList', () => {
+  it('parsers columns based on the field object', () => {
+    const result = parseSubscriberListCsv(sampleDataNormalData, sampleDataNormalFields);
+
+    expect(result).to.deep.equal(sampleDataNormalExpected);
+  })
+
+  it('if the csv is invalid, the offending rows should be returned with an error object', () => {
+  })
+})
+
