f730dc308e2bad0e10cbe34a08442362082e9f7f
4iar <me@4iar.com>
2016-09-15 17:31:00 +0100

    Implement basic file upload handling

--- client/containers/ImportSubscribers.js
+++ client/containers/ImportSubscribers.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
 
 export default class ImportSubscribers extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      file: ''
+    };
+  }
+  
+  handleUploadSuccess(file) {
+    console.log("got file");
+    console.log(file);
+    
+    this.setState({
+      file
+    });
+  }
+  
+  onUpload(file) {
+    const read = new FileReader();
+    read.readAsBinaryString(file[0]);
+    read.onloadend = function() {
+      this.handleUploadSuccess(read.result);
+    }.bind(this)
+  }
+
   render() {
     return (
       <div>
+        <Dropzone onDrop={this.onUpload.bind(this)}>
+          drag csv here
+        </Dropzone>
       </div>
     )
   }
