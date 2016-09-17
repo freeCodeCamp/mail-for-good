2087180b426ab1d3ad5eca7265bad20b1811c8dc
4iar <me@4iar.com>
2016-09-17 01:06:48 +0100

    Rename subscribers data object

--- server/controllers/addsubscribers.js
+++ server/controllers/addsubscribers.js
@@ -2,10 +2,11 @@
 
 
 module.exports =  function(req, res) {
-  const subscribers = req.body.data;
+  const subscribers = req.body.subscribers;
   const fields = req.body.fields;
   
   console.log("got new subscribers: ");
   console.log(subscribers);
+  console.log(fields);
   // -> use schema + add all the subscribers + field types to the database
 }
