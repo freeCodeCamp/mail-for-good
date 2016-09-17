36b1a76fc985accd4001355462dabcf3019232d4
4iar <me@4iar.com>
2016-09-17 15:49:53 +0100

    Modify fields and parser structure
    Previously, field names were treated as unique, now fields can have the
    same name/value, which is useful for group type fields.

--- client/utils/subscriberListParsers/parseSubscriberList.js
+++ client/utils/subscriberListParsers/parseSubscriberList.js
@@ -1,10 +1,9 @@
-import _ from 'lodash';
 import Papa from 'papaparse';
 
 export default function parseSubscriberList(inputData, fields) {
   // Parses a file (tsv/csv) to a format that can be recognised by the multiple subscriber api
   //  {
-  //    fields: { fieldname1: fieldtype1, fieldname2: fieldtype2...},
+  //    fields: [ { fieldname1: fieldtype1 }, { fieldname2: fieldtype2... }],
   //    subscribers: {
   //      { fieldname1: fieldname1_record1, fieldname2: fieldname2_record1 },
   //      { fieldname1: fieldname1_record2, fieldname2: fieldname2_record2 },
@@ -15,9 +14,15 @@ export default function parseSubscriberList(inputData, fields) {
 
   // Convert the fields to a header line that Papa can parse into a
   // value keyed object array, rather than an array of arrays
-  const header = _.keys(fields);
-
+  let header = [];
+  fields.forEach((field) => {
+    header.push(field.name)
+  })
+  
+  header = header.join(',');
+  
+  console.log(header + '\n' + inputData);
   const data = Papa.parse(header + '\n' + inputData, { header: true });
 
-  return {subscribers: data.data, fields};
+  return data.data;
 }
