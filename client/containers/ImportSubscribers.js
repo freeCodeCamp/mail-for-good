eca7b39b2de929bf8372b19ed12cd66b2e47f460
4iar <me@4iar.com>
2016-09-23 22:04:36 +0100

    Massively simplify custom field handling for subscriber csv import
    Stashed the old custom column/field handling for a later date.
    For now, fields will just be specified using the header line in a file.

--- client/containers/ImportSubscribers.js
+++ client/containers/ImportSubscribers.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
-import _ from 'lodash';
 import { connect } from 'react-redux';
 
-import deepFillArray from '../utils/deepFillArray';
-import countColumnsInFile from '../utils/countColumnsInFile';
 import parseSubscriberList from '../utils/subscriberListParsers/parseSubscriberList';
 import { addSubscribers } from '../actions/subscribersActions';
 
@@ -15,8 +12,8 @@ export default class ImportSubscribers extends React.Component {
     super();
     
     this.state = {
-      file: '',
-      fields: ['']
+      fields: null,
+      subscribers: null
     };
   }
 
@@ -29,41 +26,24 @@ export default class ImportSubscribers extends React.Component {
   }
 
   handleUploadSuccess(text) {
-    // Keep the uploaded file around for processing by the user
-    // and create an array whose elements will correspond to each column
-    // so the user can assign types/variables to the columns
-    // e.g. fields[0] (col 1) could be 'email' of type 'email'
-    // or fields[1] (col 2) could be 'favourite colour' of type 'text'
-    // or fields[2] (col 3) could be 'lucky number' of type 'int'
-    const field = {name: '', type: ''};
+    const data = parseSubscriberList(text);
+    const subscribers = data.subscribers;
+    const fields = data.fields;
+    
     this.setState({
-      file: text,
-      fields: deepFillArray(countColumnsInFile(text), field)
-    });
-  }
-
-  handleFieldNameChange(field, e) {
-    this.handleFieldChange(field, 'name', e.target.value);
-  }
-
-  handleFieldTypeChange(field, e) {
-    this.handleFieldChange(field, 'type', e.target.value);
-  }
-
-  handleFieldChange(field, selection, value) {
-    // cleaner way to do this replacement?
-    let newFields = this.state.fields;
-    newFields[field][selection] = value;
-    console.log(field);
-    console.log(selection);
-    this.setState({
-      fields: newFields
+      subscribers,
+      fields
     });
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props.addSubscribers(parseSubscriberList(this.state.file, this.state.fields), this.state.fields);
+    const newSubscribers = {
+      subscribers: this.state.subscribers,
+      fields: this.state.fields
+    };
+    
+    this.props.addSubscribers(newSubscribers);
   }
 
   render() {
@@ -73,36 +53,15 @@ export default class ImportSubscribers extends React.Component {
         <Dropzone onDrop={this.onUpload.bind(this)}>
           drag csv here
         </Dropzone>
-        <form>
-          {this.state.file && _.range(this.state.fields.length).map((field) => {
-            return (
-              <div>
-                col: {field}
-                
-                <input 
-                  type="text" 
-                  value={this.state.fields[field].name}
-                  onChange={this.handleFieldNameChange.bind(this, field)}
-                />
-
-                <select
-                  value={this.state.fields[field].type}
-                  onChange={this.handleFieldTypeChange.bind(this, field)}
-                >
-                  <option selected="selected" value="">none</option>
-                  <option value="email">email</option>
-                  <option value="number">number</option>
-                  <option value="text">text</option>
-                  <option value="group">group</option>
-                </select>
-
-                <br/>
-              </div>
-            )
-          })}
-          <button type="submit" onClick={this.handleSubmit.bind(this)}>Submit</button>
-          (should ignore name of field if it is of email type)
-        </form>
+        {this.state.subscribers && this.state.subscribers.map((subscriber) => {
+          return (
+            <div>
+              {subscriber.email}
+            </div>
+          )
+        })
+        }
+        <button type="submit" onClick={this.handleSubmit.bind(this)}>Submit</button>
       </div>
     )
   }
