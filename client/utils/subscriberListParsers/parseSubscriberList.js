27848939241678333baf16ed968aed0d793846c6
4iar <me@4iar.com>
2016-09-25 15:50:44 +0100

    Skip empty lines during csv parsing

--- client/utils/subscriberListParsers/parseSubscriberList.js
+++ client/utils/subscriberListParsers/parseSubscriberList.js
@@ -3,11 +3,11 @@ import Papa from 'papaparse';
 export default function parseSubscriberList(inputData) {
   // Parses a file (tsv/csv) into an object containing subscribers
   // and their fields
-  
-  const output = Papa.parse(inputData, { header: true });
+
+  const output = Papa.parse(inputData, { header: true, skipEmptyLines: true });
   const errors = output.errors;
   const subscribers = output.data;
   const fields = Object.keys(output.data[0]);
-  
+
   return { subscribers, fields, errors };
 }
