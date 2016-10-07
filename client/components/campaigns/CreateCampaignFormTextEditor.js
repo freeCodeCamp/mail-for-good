61bafcc60ec46f6a669de925d8c176bc9b376c1c
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-10-07 02:15:08 +0200

    Fully integrated react rte with redux form

--- client/components/campaigns/CreateCampaignFormTextEditor.js
+++ client/components/campaigns/CreateCampaignFormTextEditor.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import RichTextEditor from 'react-rte';
 
-const CreateCampaignForm = (props) => {
-    const { value, onChange } = props.input;
-    const RteEmpty = RichTextEditor.createEmptyValue();
+// Also see ref for Draft JS https://facebook.github.io/draft-js/docs/api-reference-editor.html#content
+export default class CreateCampaignForm extends Component {
+    static PropTypes = {
+        input: {
+            onChange: PropTypes.func.isRequired
+        }
+    }
 
-    return <RichTextEditor value={RteEmpty} onChange={onChange} />;
+    state = {
+        value: RichTextEditor.createEmptyValue()
+    }
+
+    onChange(value) {
+        this.setState({value});
+        this.props.input.onChange(value.toString('markdown')); // Accepts 'html' or 'markdown' - Outputs text to these formats
+    }
+
+    render() {
+        return <RichTextEditor value={this.state.value}
+            onChange={this.onChange.bind(this)}
+            placeholder="Enter your email body here"
+            spellCheck={true}
+            />;
+    }
 }
-
-export default CreateCampaignForm;
