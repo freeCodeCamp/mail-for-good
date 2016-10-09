c3412cfa1a413ffcfb1c4a07177ed4201e53cc11
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-10-09 19:26:44 +0200

    Quick fix to correct public dir for files other than HTML/JS

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -22,7 +22,7 @@ export default {
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
     path: `${__dirname}/dist`,
-    publicPath: '/dist',
+    publicPath: '/dist/',
     filename: '[name].[chunkhash].js'
   },
   plugins: [
