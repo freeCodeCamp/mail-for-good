f1e268f7ca3d18f3c31ba9e6c4dc94d879e5ba7d
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-10-10 00:03:42 +0200

    Ensure data sent to the send-campaign controller is JSON

--- client/containers/campaigns/CampaignView.js
+++ client/containers/campaigns/CampaignView.js
@@ -58,7 +58,7 @@ export default class CampaignView extends Component {
 
   sendCampaign() {
     // Send id of campaign row to server for execution @ this.state.thisCampaign.id
-    this.props.postSendCampaign();
+    this.props.postSendCampaign(JSON.stringify(this.state.thisCampaign));
   }
 
   render() {
