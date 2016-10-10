7f158ee5ee28d4e82ffacf70bbe2d78d94992f9d
4iar <me@4iar.com>
2016-10-10 19:41:00 +0100

    Add links to subscribers management page in list table

--- client/components/lists/ManageListsTable.js
+++ client/components/lists/ManageListsTable.js
@@ -1,29 +1,40 @@
 import React, { PropTypes } from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-import BSTyle from 'react-bootstrap-table/dist/react-bootstrap-table.min.css';
+import { Link } from 'react-router';
 import moment from 'moment';
+import BSTyle from 'react-bootstrap-table/dist/react-bootstrap-table.min.css';
 
 export default class ManageListsTable extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       data: this.props.data || []
     };
-    
+
     this.options = {
       noDataText: 'You do not have any lists linked with your account'
     };
   }
-  
+
   static propTypes = {
     data: PropTypes.array.isRequired
   }
-  
+
   formatFieldDate(cell) {
     return moment(cell).format('lll');
   }
-  
+
+  formatFieldManageSubscribers(cell, row) {
+    return (
+      <Link to={`/lists/manage/${row.id}`}>
+      <button type="button" className="btn btn-default btn-flat">
+          <i className="fa fa-user" />
+      </button>
+      </Link>
+    )
+  }
+
   componentWillReceiveProps(newProps) {
     this.setState({
       data: newProps.data
@@ -41,6 +52,7 @@ export default class ManageListsTable extends React.Component {
         <TableHeaderColumn dataField="name" dataSort={true}>Name</TableHeaderColumn>
         <TableHeaderColumn dataField="createdAt" dataSort={true} dataFormat={this.formatFieldDate}>Created</TableHeaderColumn>
         <TableHeaderColumn dataField="updatedAt" dataSort={true} dataFormat={this.formatFieldDate}>Updated</TableHeaderColumn>
+        <TableHeaderColumn dataAlign="center" width="150" dataFormat={this.formatFieldManageSubscribers}>Subscribers</TableHeaderColumn>
       </BootstrapTable>
     );
   }
