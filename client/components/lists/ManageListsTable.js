bea066976bf5478895d8cab0b7795937b246b917
4iar <me@4iar.com>
2016-10-10 19:22:59 +0100

    Minor aesthetic and usability changes to lists table
    Add sorting and formatting for date column (created + updated)
    
    Add sorting for name column
    
    Change case of headings for consistency

--- client/components/lists/ManageListsTable.js
+++ client/components/lists/ManageListsTable.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import BSTyle from 'react-bootstrap-table/dist/react-bootstrap-table.min.css';
+import moment from 'moment';
 
 export default class ManageListsTable extends React.Component {
   constructor(props) {
@@ -9,10 +10,18 @@ export default class ManageListsTable extends React.Component {
     this.state = {
       data: this.props.data || []
     };
+    
+    this.options = {
+      noDataText: 'You do not have any lists linked with your account'
+    };
   }
   
   static propTypes = {
     data: PropTypes.array.isRequired
+  }
+  
+  formatFieldDate(cell) {
+    return moment(cell).format('lll');
   }
   
   componentWillReceiveProps(newProps) {
@@ -25,11 +34,13 @@ export default class ManageListsTable extends React.Component {
     return (
       <BootstrapTable data={this.state.data}
                       pagination={true}
+                      options={this.options}
                       hover={true}>
+
         <TableHeaderColumn dataField="id" hidden={true} isKey={true}>id</TableHeaderColumn>
-        <TableHeaderColumn dataField="name">name</TableHeaderColumn>
-        <TableHeaderColumn dataField="createdAt">created at</TableHeaderColumn>
-        <TableHeaderColumn dataField="updatedAt">updated at</TableHeaderColumn>
+        <TableHeaderColumn dataField="name" dataSort={true}>Name</TableHeaderColumn>
+        <TableHeaderColumn dataField="createdAt" dataSort={true} dataFormat={this.formatFieldDate}>Created</TableHeaderColumn>
+        <TableHeaderColumn dataField="updatedAt" dataSort={true} dataFormat={this.formatFieldDate}>Updated</TableHeaderColumn>
       </BootstrapTable>
     );
   }
