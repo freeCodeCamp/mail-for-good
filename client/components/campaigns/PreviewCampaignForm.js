3ed0bfaa5ee430ae59db71efe4626da5d70fff96
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-10-20 03:02:58 +0200

    Functional PreviewCampaignForm
    * Add: Ability to preview a campaign before creation

--- client/components/campaigns/PreviewCampaignForm.js
+++ client/components/campaigns/PreviewCampaignForm.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 const PreviewCampaignForm = props => {
-  const { handleSubmit, lastPage, form } = props;
-  console.log(form);
+  const { handleSubmit, lastPage, form:{ values: form } } = props;
+  // { listName, campaignName, fromName, fromEmail, emailSubject, emailBody }
   return (
-    <h1>Test</h1>
+    <div>
+      <h3><i className="fa fa-list text-green" aria-hidden="true"></i> - {form.listName}</h3>
+      <h3><i className="fa fa-flag text-green" aria-hidden="true"></i> - {form.campaignName}</h3>
+
+      <hr />
+
+      <h3>From - {`${form.fromName} <${form.fromEmail}>`}</h3>
+      <h4>Subject - {`${form.emailSubject}`}</h4>
+      <blockquote>
+        <p>
+          {form.emailBody}
+        </p>
+      </blockquote>
+
+      <hr />
+
+      <button className="btn btn-lg btn-primary pull-left" type="button" onClick={lastPage}>Go back</button>
+      <button className="btn btn-lg btn-success pull-right" type="button" onClick={handleSubmit}>Create</button>
+
+    </div>
   );
 };
 
