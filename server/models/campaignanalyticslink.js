175e29b2080ca48757061cd80f5a51539046200a
4iar <me@4iar.com>
2016-10-23 00:40:28 +0100

    Implement ip-geolocation in clickthrough tracking
    This uses the offline MaxMind database and it must
    be downloaded for geolocation to work:
        $ cd node_modules/geoip-lite
        $ npm run-script updatedb
    
    This takes a little while initially but
    doesn't need to be updated often.

--- server/models/campaignanalyticslink.js
+++ server/models/campaignanalyticslink.js
@@ -1,6 +1,9 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var campaignanalyticslink = sequelize.define('campaignanalyticslink', {
+    country: DataTypes.STRING,  // 2 letter ISO-3166-1 country code
+    region: DataTypes.STRING,   // 2 char region code: US=ISO-3166-2, other=FIPS 10-4
+    city: DataTypes.STRING,     // Full city name
     ipAddress: DataTypes.STRING,
     operatingSystem: DataTypes.STRING,
     deviceType: DataTypes.STRING,
