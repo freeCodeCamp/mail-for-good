1b41aae611b644f044eb0940554dd69ae5f43cdc
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-10-25 00:52:38 +0200

    Add test server for AWS SDK SES API

--- server/controllers/campaign/email/amazon-ses/validation.js
+++
@@ -1,5 +0,0 @@
-module.exports = () => {
-  // Validation of Amazon SES emails etc may go here, though it's difficult as requests are throttled
-  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/SES.html
-
-};
