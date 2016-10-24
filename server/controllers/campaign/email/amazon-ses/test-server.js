3b3c25c2510fba5c6c751f951c3a584f8f97af04
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-10-25 01:56:30 +0200

    Functional test server
    * Add: npm script 'npm run dev' to use a local server in place of an SES server
    * Add: npm script 'npm run email:test:amazon' to start a local client at localhost:9999 that will mock responses and fake latency
    * Implementation of the test server is crude and should only be used to confirm the API request rate, possibility of buffer overflow and so on

--- server/controllers/campaign/email/amazon-ses/test-server.js
+++ server/controllers/campaign/email/amazon-ses/test-server.js
@@ -1,20 +1,23 @@
 const http = require('http');
 
+const json = JSON.stringify({
+  ResponseMetadata: {
+    RequestId: 'e8a3d6b4-94fd-11z6-afac-757cax279ap5'
+  },
+  MessageId: '01020157a1261241-90a5e1cd-3a5z-4sb7-1r41-957a4cae8e58-000000'
+});
+
 const server = http.createServer((req, res) => {
   // Mock latency
   setTimeout(() => {
-    res.writeHead({'Content-Type': 'application/json'});
-    res.end({
-      ResponseMetadata: {
-        RequestId: 'e8a3d6b4-94fd-11z6-afac-757cax279ap5'
-      },
-      MessageId: '01020157a1261241-90a5e1cd-3a5z-4sb7-1r41-957a4cae8e58-000000'
-    });
-
+    console.log('Responded!');
+    res.statusCode = 200;
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(json);
   }, 150);
 });
 
 const port = 9999;
 const host = 'localhost';
 
-server.listen(port, host, () => { console.log(`Amazon test server running at http://${host}:${port}`) });
+server.listen(port, host, () => { console.log(`Amazon test server running at http://${host}:${port}`); });
