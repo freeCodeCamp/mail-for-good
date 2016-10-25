d890c26e0a6ce1ac352032638cd7eb83f402f044
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-10-25 02:22:57 +0200

    Add notifications to Header

--- client/components/admin-lte/Header.js
+++ client/components/admin-lte/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 const Header = (props) => { // eslint-disable-line no-unused-vars
   const { user } = props;
@@ -16,6 +16,31 @@ const Header = (props) => { // eslint-disable-line no-unused-vars
         </a>
         <div className="navbar-custom-menu">
           <ul className="nav navbar-nav">
+
+          <li className="dropdown notifications-menu">
+            <a href="#" className="dropdown-toggle" data-toggle="dropdown">
+              <i className="fa fa-bell-o" />
+              <span className="label label-warning">1</span>
+            </a>
+            <ul className="dropdown-menu">
+              <li className="header">You have 1 notifications</li>
+              <li>
+                <div className="slimScrollDiv" style={{ position: 'relative', overflow: 'hidden', width: 'auto', height: '200px' }}><ul className="menu" style={{ overflow: 'hidden', width: '100%', height: '200px' }}>
+
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-users text-red" /> Example
+                    </a>
+                  </li>
+
+                </ul><div className="slimScrollBar" style={{ 'background': 'rgb(0, 0, 0) none repeat scroll 0% 0%', 'width': '3px', 'position': 'absolute', 'top': '0px', 'opacity': '0.4', 'display': 'block', 'borderRadius': '7px', 'zIndex': '99', 'right': '1px' }} />
+                <div className="slimScrollRail" style={{ 'width': '3px', 'height': '100%', 'position': 'absolute', 'top': '0px', 'display': 'none', 'borderRadius': '7px', 'background': 'rgb(51, 51, 51) none repeat scroll 0% 0%', 'opacity': '0.2', 'zIndex': '90', 'right': '1px'}} />
+                </div>
+              </li>
+              <li className="footer"><a href="#">View all</a></li>
+            </ul>
+          </li>
+
             <li className="dropdown user user-menu">
               <a className="dropdown-toggle" href="#" data-toggle="dropdown">
                 <img className="user-image" src={user.picture} alt="User Image"/>
@@ -39,6 +64,10 @@ const Header = (props) => { // eslint-disable-line no-unused-vars
       </nav>
     </header>
   );
+};
+
+Header.propTypes = {
+  user: PropTypes.object
 };
 
 export default Header;
