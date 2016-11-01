e0f9409c393ad539507465006a8448f69a529683
4iar <me@4iar.com>
2016-11-01 20:17:15 +0000

    Change host variable for consistency + refactor

--- server/controllers/campaign/email/amazon-ses/analytics.js
+++ server/controllers/campaign/email/amazon-ses/analytics.js
@@ -1,4 +1,4 @@
-const clickThroughBaseUrl = 'localhost:8080';  // placeholder
+const clickthroughHost = 'http://localhost:8080';  // placeholder
 
 function wrapLink(body, trackingId, type) {
   if (type === 'Plaintext') {  // skip link tracking for plaintext for now
@@ -6,13 +6,14 @@ function wrapLink(body, trackingId, type) {
   }
 
   body = body.replace(/\{(.+?)\/(.+?)\}/g, function(m, label, url) {
-    return '<a href="http://' + clickThroughBaseUrl + '/clickthrough?url=' + url + '&trackingId=' + trackingId + '">' + label + '</a>';
+
+    return `<a href="${clickthroughHost}/clickthrough?url=${url}&trackingId=${trackingId}">${label}</a>`
   });
   return body;
 }
 
 function insertUnsubscribeLink(body, unsubscribeId, type) {
-  const unsubscribeUrl = `http://${clickThroughBaseUrl}/unsubscribe/${unsubscribeId}`;
+  const unsubscribeUrl = `${clickthroughHost}/unsubscribe/${unsubscribeId}`;
   if (type === 'Plaintext') {
     return body + '\n' + unsubscribeUrl;
   }
@@ -26,7 +27,7 @@ function insertTrackingPixel(body, trackingId, type) {
   }
 
   return body +
-    `\n<img src="http://${clickThroughBaseUrl}/trackopen?trackingId=${trackingId}" style="position:absolute; visibility:hidden">`
+    `\n<img src="${clickthroughHost}/trackopen?trackingId=${trackingId}" style="position:absolute; visibility:hidden">`
 }
 
 module.exports = {
