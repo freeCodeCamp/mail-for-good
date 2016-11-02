16ffc9735b8fc0ec512f429eeba6cb4e16270cdf
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-11-02 23:23:27 +0100

    Add ability to export subscribed/unsubscribed users to CSV
    - Ability to filter subscribed/unsubcribed users in ManageSubscribersTable
    - Only items in the filtered table are exported to CSV

--- client/components/lists/ManageSubscribersTable.js
+++ client/components/lists/ManageSubscribersTable.js
@@ -3,7 +3,6 @@ import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import BSTyle from 'react-bootstrap-table/dist/react-bootstrap-table.min.css';
 import moment from 'moment';
 
-
 export default class ManageSubscribersTable extends React.Component {
   static propTypes = {
     data: PropTypes.array.isRequired
@@ -22,6 +21,7 @@ export default class ManageSubscribersTable extends React.Component {
   formatFieldSubscribed(subscribed) {
     // this is a placeholder for now, since we are not yet handling
     // user subscription state
+    console.log(subscribed);
     if (subscribed) {
       return '<span class="label label-success">Subscribed</span>';
     } else {
@@ -38,13 +38,27 @@ export default class ManageSubscribersTable extends React.Component {
       <BootstrapTable data={this.state.data}
                       pagination={true}
                       hover={true}
-                      exportCSV={true}>
+                      exportCSV={true}
+                      csvFileName="subscribers">
 
         <TableHeaderColumn dataField="id" hidden={true} isKey={true}>id</TableHeaderColumn>
         <TableHeaderColumn dataField="email">Email</TableHeaderColumn>
         <TableHeaderColumn dataField="createdAt" dataFormat={this.formatDate} dataSort={true} width="150">Created</TableHeaderColumn>
         <TableHeaderColumn dataField="updatedAt" dataFormat={this.formatDate} dataSort={true} width="150">Updated</TableHeaderColumn>
-        <TableHeaderColumn dataField="subscribed" dataFormat={this.formatFieldSubscribed} width="150" dataAlign="center" dataSort={true}>Status</TableHeaderColumn>
+        <TableHeaderColumn dataField="subscribed"
+          dataFormat={this.formatFieldSubscribed}
+          width="150"
+          dataAlign="center"
+          dataSort={true}
+          filter={{
+            type: 'SelectFilter',
+            options: {
+              'true': 'Subscribed',
+              'false': 'Unsubscribed'
+            }
+          }}>Status
+        </TableHeaderColumn>
+
       </BootstrapTable>
     );
   }
