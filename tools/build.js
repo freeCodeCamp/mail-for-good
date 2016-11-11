50b776f0bd13ed442fd744a42ba19ef0ee9a6fff
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-11-11 17:30:59 +0100

    Replace previous webpack build config
    - Remove previous webpack build config that bundled client side code
    - Rename existing webpack server config (which builds in the same manner as the old build config but with customisation to ensure it works with the server)
    - Previous build config could only be used by the client and had no use case given that it couldn't be used with the server and its functionality was overed by the dev config

--- tools/build.js
+++ tools/build.js
@@ -2,7 +2,7 @@
 // Allowing console calls below since this is a build file.
 /* eslint-disable no-console */
 import webpack from 'webpack';
-import config from '../webpack.config.prod';
+import config from '../webpack.config.build';
 import {chalkError, chalkSuccess, chalkWarning, chalkProcessing} from './chalkConfig';
 
 process.env.NODE_ENV = 'production'; // this assures React is built in prod mode and that the Babel dev config doesn't apply.
