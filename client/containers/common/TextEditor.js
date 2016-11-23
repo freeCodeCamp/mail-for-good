50715f04e55f3b4d75f74611b57dd98265187306
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-11-24 00:35:35 +0100

    Implement new text editor
    - Replace react-rte with a component wrapper around Trix

--- client/containers/common/TextEditor.js
+++ client/containers/common/TextEditor.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import RichTextEditor from 'react-rte';
+import ReactTrixEditor from 'react-trix-editor';
 import '../../styles/campaign-text-form.scss';
 
 export default class TextEditor extends Component {
@@ -10,36 +10,25 @@ export default class TextEditor extends Component {
 
   constructor() {
     super();
-  }
-
-  state = {
-    value: {}
-  }
-
-  componentWillMount() { // Does a value already exist for this text editor (user has already input data)?
-    this.setState({ value: this.props.input.value ? RichTextEditor.createValueFromString(this.props.input.value, 'html') : RichTextEditor.createEmptyValue() });
+    this.onChange = this.onChange.bind(this);
   }
 
   componentWillReceiveProps(props) {
     if (props.input.value && props.meta.pristine) { // Has the input prop been updated by the initialize action creator (called when applying templates)?
-      this.setState({ value: RichTextEditor.createValueFromString(props.input.value, 'html') });
+      // this.setState({ value: RichTextEditor.createValueFromString(props.input.value, 'html') });
     }
   }
 
   onChange(value) {
-    this.setState({ value });
-    this.props.input.onChange(value.toString('html'));
+    this.props.input.onChange(value);
   }
 
   render() {
     return (
-      <RichTextEditor
-      value={this.state.value}
-      onChange={this.onChange.bind(this)}
-      placeholder="Write your email"
-      spellCheck={true}
-      editorClassName="text-editor"
-      />
+      <ReactTrixEditor
+        onChange={this.onChange}
+        placeholder="Write your email"
+        />
     );
   }
 }
