ec0098f8bb52ba307ea00daa18c858f675560261
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-11-27 01:40:31 +0100

    Add transition effect

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { RouteTransition } from 'react-router-transition';
+import spring from 'react-motion/lib/spring';
 
 import Header from '../components/admin-lte/Header.js';
 import Sidebar from '../components/admin-lte/Sidebar.js';
@@ -14,6 +16,21 @@ function mapStateToProps(state) {
   };
 }
 
+
+const fadeConfig = { stiffness: 200, damping: 22 };
+
+const fade = {
+  atEnter: {
+    opacity: 0
+  },
+  atLeave: {
+    opacity: spring(0, fadeConfig)
+  },
+  atActive: {
+    opacity: spring(1, fadeConfig)
+  }
+};
+
 @connect(mapStateToProps, { emitProfileRequest, consumeNotification })
 export default class App extends Component {
 
@@ -22,7 +39,13 @@ export default class App extends Component {
     emitProfileRequest: PropTypes.func.isRequired,
     consumeNotification: PropTypes.func.isRequired,
     user: PropTypes.object,
-    ws_notification: PropTypes.array.isRequired
+    ws_notification: PropTypes.array.isRequired,
+    route: React.PropTypes.object,
+    location: React.PropTypes.object
+  }
+
+  static contextTypes = {
+    router: React.PropTypes.object
   }
 
   componentWillMount() {
@@ -36,7 +59,12 @@ export default class App extends Component {
         <Sidebar user={this.props.user} />
 
         <div className="content-wrapper">
+          <RouteTransition
+          pathname={this.props.location.pathname}
+          {...fade}
+          >
           {this.props.children}
+        </RouteTransition>
         </div>
 
         <Notifications />
