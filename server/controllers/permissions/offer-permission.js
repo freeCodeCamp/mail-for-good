9bf8bc3ae188aebe03106f10ac446b3bdb6fdf0b
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-12-10 23:26:14 +0100

    Functional offer-permissions controller

--- server/controllers/permissions/offer-permission.js
+++ server/controllers/permissions/offer-permission.js
@@ -1,5 +1,44 @@
 const OfferPermission = require('../../models').offerPermission;
+const User = require('../../models').user;
 
 module.exports = function(req, res) {
-  console.log(req.body);
-}
+
+  const { email, campaigns } = req.body;
+
+  User.findOne({
+    where: {
+      email
+    }
+  })
+  .then(userInstance => {
+    if(!userInstance) {
+      res.status(400).send({ message: 'This user does not exist' });
+    } else {
+
+      OfferPermission.findOrCreate({
+        where: {
+          userId: req.user.id,
+          toUserId: String(userInstance.getDataValue('id'))
+        },
+        defaults: {
+          toUserId: String(userInstance.getDataValue('id')),
+          campaigns
+        }
+      })
+      .then((opInstance, created) => {
+        if (created) {
+          res.status(400).send({ message: 'You have already granted this user permissions. If you wish to change them, please delete them first' });
+        } else {
+          res.send({ message: 'Your offer to grant this user these permissions has been sent' });
+        }
+      })
+      .catch(err => {
+        throw err;
+      });
+
+    }
+  })
+  .catch(err => {
+    throw err;
+  });
+};
--- server/models/offerPermission.js
+++ server/models/offerPermission.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var offerPermission = sequelize.define('offerPermission', {
-    toUserEmail: DataTypes.STRING,
+    toUserId: DataTypes.STRING,
     campaigns: DataTypes.STRING
   }, {
     classMethods: {
