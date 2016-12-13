690e03633fc2b89cad652b5db05d809380f627f5
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2016-12-13 07:03:41 +0100

    Forbid offering a user permissions when existing permissions for this user exist

--- server/controllers/permissions/grant-permission.js
+++ server/controllers/permissions/grant-permission.js
@@ -1,4 +1,5 @@
 const OfferPermission = require('../../models').offerPermission;
+const ACL = require('../../models').acl;
 const User = require('../../models').user;
 
 module.exports = function(req, res) {
@@ -15,29 +16,47 @@ module.exports = function(req, res) {
       res.status(400).send({ message: 'This user does not exist' });
     } else {
 
-      OfferPermission.findOne({
+      ACL.findOne({
         where: {
-          userId: req.user.id,
+          userId: String(req.user.id),
           toUserId: String(userInstance.getDataValue('id'))
         }
       })
-      .then(opInstance => {
-
-        if (opInstance) {
+      .then(aclInstance => {
+        if (aclInstance) {
+          // User has already been granted permissions
           res.status(400).send({ message: 'You have already granted this user permissions. If you wish to change them, please delete them first' });
         } else {
-          OfferPermission.create({
-            userId: req.user.id,
-            toUserId: String(userInstance.getDataValue('id')),
-            toUserEmail: email,
-            campaigns
+
+          OfferPermission.findOne({
+            where: {
+              userId: req.user.id,
+              toUserId: String(userInstance.getDataValue('id'))
+            }
           })
-          .then(() => res.send({ message: 'Your offer to grant this user these permissions has been sent' }))
+          .then(opInstance => {
+
+            if (opInstance) {
+              res.status(400).send({ message: 'You have already offered this user permissions. If you wish to change them, please delete them first' });
+            } else {
+              OfferPermission.create({
+                userId: req.user.id,
+                toUserId: String(userInstance.getDataValue('id')),
+                toUserEmail: email,
+                campaigns
+              })
+              .then(() => res.send({ message: 'Your offer to grant this user these permissions has been sent' }))
+              .catch(err => {
+                throw err;
+              });
+            }
+
+          })
           .catch(err => {
             throw err;
           });
-        }
 
+        }
       })
       .catch(err => {
         throw err;
