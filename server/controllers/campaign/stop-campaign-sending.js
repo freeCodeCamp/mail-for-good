bd35c1fe59f116787648faf7ff129bc37f6ce6de
4iar <me@4iar.com>
2016-12-13 19:01:41 +0000

    Validate that campaign belongs to user

--- server/controllers/campaign/stop-campaign-sending.js
+++ server/controllers/campaign/stop-campaign-sending.js
@@ -1,10 +1,30 @@
-module.exports = (req, res) => {
+const {
+  sequelize,
+  campaign: Campaign,
+  user: User,
+} = require('../../models');
+
+
 
   // If req.body.id was not supplied or is not a number, cancel
   if (!req.body.id || typeof req.body.id !== 'number') {
     res.status(400).send();
     return;
   }
+  const userId = req.user.id;
+  const campaignId = req.body.id;
 
-  res.send({ }):
+  Campaign.findOne({
+    where: {
+      userId,
+      id: campaignId
+    },
+    raw: true
+  }).then(campaign => {
+    if (campaign) {
+      res.send();
+    } else {
+      res.status(400).send();
+    }
+  });
 }
--- server/controllers/campaign/stop-campaign-sending.spec.js
+++ server/controllers/campaign/stop-campaign-sending.spec.js
@@ -1,4 +1,6 @@
 const { expect } = require('chai');
+const httpMocks = require('node-mocks-http');
+
 const stopCampaignSending = require('./stop-campaign-sending');
 const {
   sequelize,
@@ -7,6 +9,19 @@ const {
 } = require('../../models');
 
 describe('stopCampaignSending', () => {
+  beforeEach(done => {
+    sequelize.sync({ force: true }).then(() => {
+      User.create({id: 1}).then(user => {
+        Campaign.create({
+          name: 'campaign1',
+          userId: 1
+        }).then(campaign => {
+          done();
+        });
+      });
+    });
+  });
+
   xit('validates that campaign id is present in request body', () => {
   });
 
@@ -16,6 +31,17 @@ describe('stopCampaignSending', () => {
   xit('sends a success message after redis flags have been set', () => {
   });
 
-  xit('validates that the campaign belongs to the user', () => {
+  it('validates that the campaign belongs to the user', (done) => {
+    const res = httpMocks.createResponse({ eventEmitter: require('events').EventEmitter });
+    const req = {
+      user: {id: 1337},
+      body: {id: 1}
+    };
+
+    stopCampaignSending(req, res);
+    res.on('finish', () => {
+      expect(res.statusCode).to.be.equal(400);
+      done();
+    });
   });
 });
