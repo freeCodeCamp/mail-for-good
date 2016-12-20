b35f930a1092bb882e95f5eda7d363081e99114e
4iar <me@4iar.com>
2016-12-20 21:48:23 +0000

    Add complaint, delivered, and bounce series

--- client/components/campaigns/ManageCampaignsGraph.js
+++ client/components/campaigns/ManageCampaignsGraph.js
@@ -22,22 +22,43 @@ export default class ManageCampaignsGraph extends React.Component {
   }
 
   render() {
-    let xAxis = [];
-    let data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let sentData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let deliveredData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let complaintData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let bounceData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
     this.state.data.forEach(campaign => {
       const monthUTC = campaign.createdAt.getUTCMonth();
-      data[monthUTC] += campaign['campaignanalytic.totalSentCount'];
+      sentData[monthUTC] += campaign['campaignanalytic.totalSentCount'];
+      complaintData[monthUTC] += campaign['campaignanalytic.complaintCount'];
+      bounceData[monthUTC] += campaign['campaignanalytic.permanentBounceCount'];
+
+      deliveredData[monthUTC] += campaign['campaignanalytic.total'] -
+        campaign['campaignanalytic.permanentBounceCount'] -
+        campaign['campaignanalytic.transientBounceCount'];;
     })
 
     var config = {
+      title: {
+        text: 'Campaign Stats'
+      },
       xAxis: {
+        title: { text: 'Date' },
         categories: months
       },
-      series: [{
-        data
-      }]
+      yAxis: {
+        title: {
+          text: 'Emails'
+        },
+        min: 0
+      },
+      series: [
+        { name: 'Sent', data: sentData },
+        { name: 'Complaint', data: complaintData },
+        { name: 'Delivered', data: deliveredData },
+        { name: 'Bounced', data: bounceData }
+        ]
     };
 
     return (
