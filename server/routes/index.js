23c9f494aaa1b0e35e82a1d22e746095f75bb4d4
drewwalsh1@gmail.com <drewwalsh1@gmail.com>
2017-01-19 03:34:12 +0100

    Add permissions route

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,21 +5,6 @@ const parseJson = bodyParser.json();
 
 const unsubscribe = require('../controllers/subscriber/unsubscribe');
 
-// Permissions
-const getGrantedPermissions = require('../controllers/permissions/get-granted-permissions');
-const grantPermissions = require('../controllers/permissions/grant-permission');
-const deleteGrantedPermissions = require('../controllers/permissions/delete-granted-permissions');
-
-const getActivePermissions = require('../controllers/permissions/get-active-permissions');
-const deleteActivePermissions = require('../controllers/permissions/delete-active-permissions');
-
-const getReceivedPermissionOffers = require('../controllers/permissions/get-received-permission-offers');
-const acceptPermissionOffer = require('../controllers/permissions/accept-permission-offer');
-const rejectPermissionOffer = require('../controllers/permissions/reject-permission-offers');
-
-const getGrantOfferedPermissions = require('../controllers/permissions/get-grant-offered-permissions');
-const deleteGrantOfferedPermissions = require('../controllers/permissions/delete-grant-offered-permissions');
-
 // Analytics
 const getClickthroughs = require('../controllers/analytics/get-clickthroughs');
 const refresh = require('../controllers/analytics/refresh');
@@ -40,6 +25,7 @@ const { apiIsAuth, isAuth } = require('./middleware/auth');
 const lists = require('./lists');
 const templates = require('./templates');
 const campaigns = require('./campaigns');
+const permissions = require('./permissions');
 
 module.exports = (app, passport, io, redis) => {
 
@@ -68,49 +54,7 @@ module.exports = (app, passport, io, redis) => {
   lists(app, io);
 
   /* Permissions */
-  // Get granted permissions (i.e. permissions you have granted another user)
-  app.get('/api/permissions', apiIsAuth, (req, res) => {
-    getGrantedPermissions(req, res);
-  });
-  // Post to offer another user a set of permissions
-  app.post('/api/permissions', apiIsAuth, parseJson, (req, res) => {
-    grantPermissions(req, res);
-  });
-  // Delete granted permissions, removes item(s) from the ACL
-  app.delete('/api/permissions', apiIsAuth, parseJson, (req, res) => {
-    deleteGrantedPermissions(req, res);
-  });
-
-  // Get active permissions (grantee -> granter)
-  app.get('/api/active-permissions', apiIsAuth, (req, res) => {
-    getActivePermissions(req, res);
-  });
-  // Delete active permissions, removes item(s) from the ACL
-  app.delete('/api/active-permissions', apiIsAuth, parseJson, (req, res) => {
-    deleteActivePermissions(req, res);
-  });
-
-  // Get received permission offers
-  app.get('/api/received-permissions', apiIsAuth, (req, res) => {
-    getReceivedPermissionOffers(req, res);
-  });
-  // Post to accept permission offers
-  app.post('/api/received-permissions', apiIsAuth, parseJson, (req, res) => {
-    acceptPermissionOffer(req, res);
-  });
-  // Delete to reject permission offers
-  app.delete('/api/received-permissions', apiIsAuth, parseJson, (req, res) => {
-    rejectPermissionOffer(req, res);
-  });
-
-  // Get offered permissions (granter -> grantee)
-  app.get('/api/grant-offered-permissions', apiIsAuth, (req, res) => {
-    getGrantOfferedPermissions(req, res);
-  });
-  // Delete offered permissions, removes item(s) from the ACL
-  app.delete('/api/grant-offered-permissions', apiIsAuth, parseJson, (req, res) => {
-    deleteGrantOfferedPermissions(req, res);
-  });
+  permissions(app);
 
   /* Settings */
   // Get boolean values designating assigned fields
