d0e434b6f61ab7d68fd59ea5ef7513af771058a0
4iar <me@4iar.com>
2017-01-31 18:02:39 +0000

    Automatically configure AWS for email feedback
    Now the SQS queue will automatically be configured to receive email
    feedback messages, so a user no longer needs to configure and provide
    the SQS queue url.

--- server/controllers/settings/changesettings.js
+++ server/controllers/settings/changesettings.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const Setting = require('../../models').setting;
+const configureAws = require('./configure-aws/configure-aws');
 
 module.exports = function(req, res) {
   const settingsToChange = _.pickBy(req.body);
@@ -12,8 +13,6 @@ module.exports = function(req, res) {
 
   /*
     NOTE: There's no need to include any response message as the settings are validated client side
-    TODO: Need to encrypt these keys
-    TODO: Validate SQS url
  */
 
   // Quick function to create a new object selectProvidedFields that contains keys from settingsToChange whose property was not an empty string (or otherwise falsey value)
@@ -24,23 +23,38 @@ module.exports = function(req, res) {
     }
   });
 
-  // All checks below are pretty self explanatory
-  if (selectProvidedFields.accessKey) {
-    if (selectProvidedFields.accessKey.length < 16 || selectProvidedFields.accessKey.length > 32) {
-      res.status(400).send();
-      return;
-    }
-  }
+  // If any AWS settings have been changed then we need to reconfigure the feedback queues
+  // This also serves as validation for AWS credentials
+  if (selectProvidedFields.amazonSimpleEmailServiceAccessKey || selectProvidedFields.amazonSimpleEmailServiceSecretKey || selectProvidedFields.region) {
+    console.log("aws settings provided")
+    Setting.findOne({
+      where: { userId: req.user.id },
+      attributes: ['amazonSimpleEmailServiceAccessKey', 'amazonSimpleEmailServiceSecretKey', 'region']
+    }).then(settingInstance => {
+      const settings = _.extend(
+        {
+          amazonSimpleEmailServiceAccessKey: settingInstance.amazonSimpleEmailServiceAccessKey,
+          amazonSimpleEmailServiceSecretKey: settingInstance.amazonSimpleEmailServiceSecretKey,
+          region: settingInstance.region
+        }, selectProvidedFields);
 
-  if (selectProvidedFields.secretAccessKey) {
-    if (selectProvidedFields.secretAccessKey.length < 40) {
-      res.status(400).send();
-      return;
-    }
-    else if (/[0-9a-f]{40}/.test(selectProvidedFields.secretAccessKey)) {
-      res.status(400).send();
-      return;
-    }
+      configureAws({
+        accessKey: settings.amazonSimpleEmailServiceAccessKey,
+        secretKey: settings.amazonSimpleEmailServiceSecretKey,
+        region: settings.region
+      }, (err, queueUrl) => {
+        if (err) {
+          // Should add error messages here
+          res.status(400).send();
+        } else {
+          console.log(`Created sqs feedback queue: ${queueUrl}`);
+          selectProvidedFields.amazonSimpleQueueServiceUrl = queueUrl;
+          updateSettings();
+        }
+      });
+    });
+  } else {
+    updateSettings()
   }
 
   if (selectProvidedFields.regions) {
@@ -51,13 +65,38 @@ module.exports = function(req, res) {
     }
   }
 
-  Setting.update(selectProvidedFields, {
-    where: {
-      userId: req.user.id
-    }
-  }).then(() => {
-    res.send();
-  }).catch(() => {
-    res.status(500).send();
-  });
+  function updateSettings () {
+    Setting.update(selectProvidedFields, {
+      where: {
+        userId: req.user.id
+      }
+    }).then(() => {
+      res.send();
+    }).catch(() => {
+      res.status(500).send();
+    });
+  }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
