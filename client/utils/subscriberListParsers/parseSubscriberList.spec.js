da3a872349f34e34a758cab6fdaa8e6362e2b407
AndrewGHC <drewwalsh1@gmail.com>
2017-02-19 03:46:13 +0100

    Fix client testing for parseSubscriberList

--- client/utils/subscriberListParsers/parseSubscriberList.spec.js
+++ client/utils/subscriberListParsers/parseSubscriberList.spec.js
@@ -8,15 +8,48 @@ khanson5@amazon.co.jp,Male,Keith,Hanson,California,Pass
 jgrant6@hexun.com,Female,Julia,Grant,Georgia,Drive`;
 
 const sampleDataNormalExpected = {
-  fields: ['email', 'gender', 'firstName', 'lastName', 'state', 'streetType'],
+  data:
+   [ { email: 'nreid0@domainmarket.com',
+       gender: 'Female',
+       firstName: 'Nancy',
+       lastName: 'Reid',
+       state: 'Virginia',
+       streetType: 'Plaza' },
+     { email: 'bbrooks1@about.me',
+       gender: 'Female',
+       firstName: 'Bonnie',
+       lastName: 'Brooks',
+       state: 'Florida',
+       streetType: 'Circle' },
+     { email: 'khanson5@amazon.co.jp',
+       gender: 'Male',
+       firstName: 'Keith',
+       lastName: 'Hanson',
+       state: 'California',
+       streetType: 'Pass' },
+     { email: 'jgrant6@hexun.com',
+       gender: 'Female',
+       firstName: 'Julia',
+       lastName: 'Grant',
+       state: 'Georgia',
+       streetType: 'Drive' } ],
   errors: [],
-  subscribers: [
-    {email: 'nreid0@domainmarket.com', gender: 'Female', firstName: 'Nancy', lastName: 'Reid', state: 'Virginia', streetType: 'Plaza'},
-    {email: 'bbrooks1@about.me', gender: 'Female', firstName: 'Bonnie', lastName: 'Brooks', state: 'Florida', streetType: 'Circle'},
-    {email: 'khanson5@amazon.co.jp', gender: 'Male', firstName: 'Keith', lastName: 'Hanson', state: 'California', streetType: 'Pass'},
-    {email: 'jgrant6@hexun.com', gender: 'Female', firstName: 'Julia', lastName: 'Grant', state: 'Georgia', streetType: 'Drive'},
-  ]
+  meta:
+   { delimiter: ',',
+     linebreak: '\n',
+     aborted: false,
+     truncated: false,
+     cursor: 266,
+     fields:
+      [ 'email',
+        'gender',
+        'firstName',
+        'lastName',
+        'state',
+        'streetType' ]
+    }
 };
+
 
 const sampleDataInvalidData = `email,gender,firstName,lastName,state,streetType
 nreid0@domainmarket.com,Female,Nancy,Reid,Virginia
@@ -30,13 +63,19 @@ const sampleDataInvalidExpectedErrors = [
 ];
 
 describe('parseSubscriberList', () => {
-  it('parsers a csv into an object containing subscribers and their fields', () => {
-    const result = parseSubscriberListCsv(sampleDataNormalData);
-    expect(result).to.deep.equal(sampleDataNormalExpected);
+  it('parses a csv into an object containing subscribers and their fields', () => {
+    function mockCallback(result) {
+      expect(result).to.deep.equal(sampleDataNormalExpected);
+    }
+
+    parseSubscriberListCsv(sampleDataNormalData, mockCallback);
   });
 
   it('if the csv is invalid, the offending rows should be returned with an error object', () => {
-    const result = parseSubscriberListCsv(sampleDataInvalidData);
-    expect(result.errors).to.deep.equal(sampleDataInvalidExpectedErrors);
+    function mockCallback(result) {
+      expect(result.errors).to.deep.equal(sampleDataInvalidExpectedErrors);
+    }
+
+    const result = parseSubscriberListCsv(sampleDataInvalidData, mockCallback);
   });
 });
