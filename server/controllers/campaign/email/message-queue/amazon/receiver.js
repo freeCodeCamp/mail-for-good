719a6b62ceffbfd4753517049c8c07c42413ec70
AndrewGHC <drewwalsh1@gmail.com>
2017-03-05 01:41:49 +0200

    Fix queue locking issues & campaign sending woes.

--- server/controllers/campaign/email/message-queue/amazon/receiver.js
+++ server/controllers/campaign/email/message-queue/amazon/receiver.js
@@ -1,6 +1,5 @@
 const Queue = require('bull');
-const Receiver = Queue('amazon', null, process.env.REDIS_HOST || '127.0.0.1');
-const Limiter = require('rolling-rate-limiter');
+const Receiver = Queue('amazon', process.env.REDIS_PORT || 6379, process.env.REDIS_HOST || '127.0.0.1');
 const Promise = require('bluebird');
 const redis = require("redis");
 
@@ -13,44 +12,24 @@ const CampaignAnalytics = require('../../../../../models').campaignanalytics;
 */
 
 module.exports = function(ses, rateLimit, campaignInfo) {
-  const EMAILS_PER_SECOND = rateLimit * 1000;
-  const ONE_SECOND = 1000;
-  const LIMITER_NAMESPACE = 'email'; // Variable used by Limiter to determine if an item should be limited
+  // const EMAILS_PER_SECOND = rateLimit;
+  // const ONE_SECOND = 1000;
   const CONCURRENCY = rateLimit; // No. of jobs to process in parallel on a single worker
 
-  Receiver.process(CONCURRENCY, (job, done) => {
+  Receiver.process(CONCURRENCY, job => {
     // Call the _sendEmail function in the parent closure
     const { email, task } = job.data; // See Amazon.js - where { email } is a formatted SES email & { info } contains the id
-    return ses.sendEmail(email, (err, data) => {
-      if (err) {
-        console.log(err); // eslint-disable-line
-      } else {
-        const p1 = CampaignSubscriber.update(
-          {
-            messageId: data.MessageId,
-            sent: true
-          },
-          {
-            where: {
-              id: task['campaignsubscribers.id'],
-              listsubscriberId: task.id,
-              campaignId: campaignInfo.campaignId
-            },
-            limit: 1
-          }
-        );
 
-        const p2 = CampaignAnalytics.findById(campaignInfo.campaignAnalyticsId)
-          .then(foundCampaignAnalytics => {
-            return foundCampaignAnalytics.increment('totalSentCount');
-          });
-
-        done();
-
-        return Promise.all([p1, p2]);
-        // _updateAnalytics(data, emailFormat.id);
-      }
-    });
+    // Promisify (turn into a promise) the send email callback func - http://bluebirdjs.com/docs/api/promise.promisify.html
+    const promisifiedSes = Promise.promisify(ses.sendEmail, { context: ses });
+    return promisifiedSes(email)
+      .then(data => {
+        return _updateAnalytics(data, task, campaignInfo);
+      })
+      .catch(err => {
+        console.log(err);
+        return;
+      });
   });
 
   Receiver.on('completed', function(){
@@ -63,55 +42,31 @@ module.exports = function(ses, rateLimit, campaignInfo) {
     console.log(error); // eslint-disable-line
   });
 
-
   const redisSettings = { host: process.env.REDIS_HOST || '127.0.0.1' };
   const client = redis.createClient(redisSettings);
 
-  const limiter = Promise.promisify(Limiter({
-    interval: ONE_SECOND,
-    maxInInterval: EMAILS_PER_SECOND,
-    redis: client,
-    namespace: `amazon-${Math.random().toString(36)}`
-  }));
-
-  function _updateAnalytics(data, listsubscriberId) {
-    CampaignSubscriber.update(
+  function _updateAnalytics(data, task, campaignInfo) {
+    const p1 = CampaignSubscriber.update(
       {
         messageId: data.MessageId,
         sent: true
       },
       {
         where: {
-          listsubscriberId,
+          id: task['campaignsubscribers.id'],
+          listsubscriberId: task.id,
           campaignId: campaignInfo.campaignId
         },
         limit: 1
       }
     );
 
-    CampaignAnalytics.findById(campaignInfo.campaignAnalyticsId)
+    const p2 = CampaignAnalytics.findById(campaignInfo.campaignAnalyticsId)
       .then(foundCampaignAnalytics => {
-        foundCampaignAnalytics.increment('totalSentCount');
+        return foundCampaignAnalytics.increment('totalSentCount');
       });
-  }
 
-  function _sendEmail(emailFormat) {
-    limiter(LIMITER_NAMESPACE, function(thereIsTimeLeft) {
-      if (thereIsTimeLeft) {
-        // limit was exceeded, action should not be allowed
-        //// Retry in a second.
-        console.log('Jobs are being sent too fast!'); // eslint-disable-line
-        Promise.delay(ONE_SECOND).then(() => _sendEmail(emailFormat));
-      } else {
-        // limit was not exceeded, action should be allowed
-        ses.sendEmail(emailFormat, (err, data) => {
-          if (err) {
-            console.log(err); // eslint-disable-line
-          }
-          _updateAnalytics(data, emailFormat.id);
-        });
-      }
-    });
+    return Promise.all([p1, p2]);
   }
 
   function _quit() {
