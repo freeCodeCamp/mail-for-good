57937fa87cf8c8a5e7b01e228565cafccefd6c8d
4iar <me@4iar.com>
2017-04-19 15:23:56 +0100

    Consume feedback messages during sending

--- server/feedback-consumer/consumer.js
+++ server/feedback-consumer/consumer.js
@@ -7,42 +7,17 @@ const feedbackConsumer = require('./feedback-consumer');
 const Campaign = require('../models').campaign;
 
 
-var isSending = false;
-
 feedbackConsumer.start();
+
+console.log("Feedback consumer started: watching for feedback messages (bounces, complaints, etc) from SQS");
 
 // Restart the feedback consumers to apply new credentials when
 // new settings are received
 const subscriber = redis.createClient();
 subscriber.on('message', (channel, event) => {
-  if (event == 'changed' && !isSending) {
+  if (event == 'changed') {
     debug('Got a change-settings event, so restarting consumers to apply new credentials');
     feedbackConsumer.restart();
   }
 });
 subscriber.subscribe('change-settings');
-
-// Poll the database to check that campaigns are not being sent.
-// If a campaign is being sent, stop the feedback consumers
-// to speed up email sending (by reducing writes to the database)
-// and prevent race conditions caused by receiving a feedback message
-// before the messageId has been stored.
-const pollingRateMs = 10000;
-async.forever(next => {
-    Campaign.count({
-      where: { status: 'sending' }
-    }).then(numSending => {
-      if (numSending) {
-        debug('%d campaigns are being sent, stopping feedback consumer', numSending);
-        isSending = true;
-        feedbackConsumer.stop();
-      } else {
-        debug('No campaigns are being sent, restarting feedback consumer');
-        isSending = false;
-        feedbackConsumer.restart();
-      }
-      setTimeout(next, pollingRateMs);
-    }).catch(err => {
-      throw err;
-    });
-});
