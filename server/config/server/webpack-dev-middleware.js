d2954faa7a805cab7e49896a87bd4958418c4557
AndrewGHC <drewwalsh1@gmail.com>
2017-07-13 03:40:29 +0100

    Dev server middleware configured in line with best practice

--- server/config/server/webpack-dev-middleware.js
+++ server/config/server/webpack-dev-middleware.js
@@ -1,4 +1,10 @@
 const webpack = require('webpack');
+const history = require('connect-history-api-fallback');
+
+/*
+  With thanks to https://github.com/coryhouse/react-slingshot
+  This app was kickstarted with this generator.
+*/
 
 module.exports = app => {
   if (process.env.NODE_ENV === 'development') {
@@ -8,7 +14,21 @@ module.exports = app => {
     const devWebpackConfig = require('../../../webpack.config.dev');
     const compiler = webpack(devWebpackConfig);
 
-    app.use(webpackDevMiddleware(compiler, { publicPath: devWebpackConfig.output.publicPath }));
+    app.use(history());
+    app.use(webpackDevMiddleware(compiler, {
+      publicPath: devWebpackConfig.output.publicPath,
+      noInfo: false,
+      quiet: false,
+      stats: {
+        assets: false,
+        colors: true,
+        version: false,
+        hash: false,
+        timings: false,
+        chunks: false,
+        chunkModules: false
+      },
+    }));
     app.use(webpackHotMiddleware(compiler));
   }
 };
