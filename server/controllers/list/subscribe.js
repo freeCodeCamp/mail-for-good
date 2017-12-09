9be693c5dd245134c53957bf689898d8307150d3
Valentin Dupas <valentin.lc.dupas@gmail.com>
2017-12-09 01:13:34 +0100

    Subscription form can now redirect to custom URLs on success and failure

--- server/controllers/list/subscribe.js
+++ server/controllers/list/subscribe.js
@@ -4,9 +4,13 @@ const List = require('../../models').list;
 module.exports = function(req, res) {
   const email = req.query.email;
   const subscribeKey = req.query.subscribeKey;
+  const redirectOnSuccess = req.query.redirectOnSuccess;
+  const redirectOnFailure = req.query.redirectOnFailure;
 
   List.findOne({
     where: { subscribeKey }
+  }).catch(error => {
+    console.error(error);
   }).then(list => {
     if (list) {
       const listId = list.dataValues.id;
@@ -22,27 +26,34 @@ module.exports = function(req, res) {
           ListSubscriber.update(
             { subscribed: true },
             { where: { id: listSubscriber.id } }
-          ).then(() => {
-            res.status(201)
-              .send({
-                status: 'success', // Redundant
-                message: `added successfully`
-              });
-          });
+          ).then(handleSuccess(res,redirectOnSuccess));
         } else {
           ListSubscriber.create({
             email, listId
-          }).then(() => {
-            res.status(201)
-              .send({
-                status: 'success', // Redundant
-                message: `added succesfully`
-              });
-          });
+          }).then(handleSuccess(res,handleSuccess));
         }
       });
     } else {
-      res.status(400).send('invalid list subscriber key');
+      handleFailure(res,redirectOnFailure);
     }
   });
 };
+
+function handleSuccess(res,redirectOnSuccess){
+  if(redirectOnSuccess!==''){
+    res.redirect(redirectOnSuccess);
+  }else{
+    res.status(201).send({
+        status: 'success', // Redundant
+        message: `added successfully`
+    });
+  }
+}
+
+function handleFailure(res,redirectOnFailure){
+  if(redirectOnFailure!==''){
+    res.redirect(redirectOnFailure);
+  }else{
+    res.status(400).send('invalid list subscriber key');
+  }
+}
