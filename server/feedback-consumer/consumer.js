41825838974367be79d652fd2895737881d7aac0
Valentin Dupas <valentin.lc.dupas@gmail.com>
2018-01-30 04:03:21 +0100

    can now connect to remote redis using REDIS_URL env variable

--- server/feedback-consumer/consumer.js
+++ server/feedback-consumer/consumer.js
@@ -6,6 +6,13 @@ const async = require('async');
 const feedbackConsumer = require('./feedback-consumer');
 const Campaign = require('../models').campaign;
 
+let redisSettings = null;
+if(process.env.HEROKU){
+  redisSettings = {url: process.env.REDIS_URL}
+}else{
+  redisSettings = { host: process.env.REDIS_HOST || '127.0.0.1' }
+}
+
 
 feedbackConsumer.start();
 
@@ -13,7 +20,7 @@ console.log("Feedback consumer started: watching for feedback messages (bounces,
 
 // Restart the feedback consumers to apply new credentials when
 // new settings are received
-const subscriber = redis.createClient({ host: process.env.REDIS_HOST || '127.0.0.1' });
+const subscriber = redis.createClient(redisSettings);
 subscriber.on('message', (channel, event) => {
   if (event == 'changed') {
     debug('Got a change-settings event, so restarting consumers to apply new credentials');
@@ -22,8 +29,8 @@ subscriber.on('message', (channel, event) => {
 });
 subscriber.subscribe('change-settings');
 
-const pollingRateMs = 10000;        
-async.forever(next => {       
+const pollingRateMs = 10000;
+async.forever(next => {
     feedbackConsumer.restart();
-    setTimeout(next, pollingRateMs);     
+    setTimeout(next, pollingRateMs);
 });
