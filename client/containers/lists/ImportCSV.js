da7f40be54c1cfd3cfbd592eb52a5b53b384b20c
Luke Camelo <azathothbeats@gmail.com>
2018-07-24 18:13:25 -0400

    CSV Import no longer requires 'email' to be case sensitive, can be written with a dash e.g. E-mail

--- client/containers/lists/ImportCSV.js
+++ client/containers/lists/ImportCSV.js
@@ -46,8 +46,8 @@ export class ImportCSVComponent extends Component {
   handleNewFile(file) {
 
     const callback = results => {
-      if (!results.meta.fields.some(field => field.toLowerCase() === 'email')) { // Check if any header field is labeled email
-        this.handleErrorWithFile('Please ensure the CSV file contains at least one column field labeled "email" (check the first row)');
+      if (!results.meta.fields.some(field => field.toLowerCase() === 'email' || 'e-mail')) { // Check if any header field is labeled email
+        this.handleErrorWithFile('Please ensure the CSV file contains at least one column field labeled "email/e-mail" (check the first row)');
       } else {
 
         const errors = results.errors;
